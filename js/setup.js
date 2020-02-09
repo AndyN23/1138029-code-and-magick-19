8af178c2b0635f09be52e8600e65a4f2f6c92c1a
AndyN23 <a.y.nesterov@yandex.ru>
2020-02-10 00:51:10 +0300

    Обработка событий клик/нажатие кнопки - смена цвета, открытие/закрытие попап

--- js/setup.js
+++ js/setup.js
@@ -1,10 +1,10 @@
 'use strict';
 
-document.querySelector('.setup').classList.remove('hidden');
+// document.querySelector('.setup').classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');
 
 var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+//userDialog.classList.remove('hidden');
 
 var similarListElement = document.querySelector('.setup-similar-list');
 
@@ -49,3 +49,126 @@ for (i = 0; i < wizards.length; i++) {
 similarListElement.appendChild(fragment);
 
 document.querySelector('.setup-similar').classList.remove('hidden');
+
+
+
+
+
+
+
+//_________________________________________________________________
+// Задание обработка событий
+
+var ESC_KEY = 'Escape';
+var ENTER_KEY = 'Enter';
+
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+
+var setupWizard =  document.querySelector('.setup-wizard');
+var wizardCoat = setupWizard.querySelector('.wizard-coat');
+var wizardEyes = setupWizard.querySelector('.wizard-eyes');
+var setupFireballWrap =document.querySelector('.setup-fireball-wrap');
+
+var FIREBALL_COLOR = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
+wizardCoat.addEventListener('click', function () {
+  wizardCoat.style.fill = getRandomValue(COAT_COLOR);
+});
+
+wizardEyes.addEventListener('click', function () {
+  wizardEyes.style.fill = getRandomValue(EYES_COLOR);
+});
+
+setupFireballWrap.addEventListener('click', function (){
+  setupFireballWrap.style.backgroundColor = getRandomValue(FIREBALL_COLOR);
+});
+
+var onPopupEscPress = function (evt) {
+  if (evt.key === ESC_KEY) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+
+  document.addEventListener('keydown', onPopupEscPress);
+  /* function (evt) {
+    if (evt.key === 'Escape') {
+      closePopup(); */
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+// setupOpen.addEventListener('click', function () {
+//   setup.classList.remove('hidden');
+
+//   document.addEventListener('keydown', function (evt) {
+//     if (evt.key === 'Escape') {
+//       setup.classList.add('hidden');
+//     }
+//   });
+// });
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    openPopup();
+  }
+});
+// setupOpen.addEventListener('keydown', function (evt) {
+//   if (evt.key === 'Enter') {
+//     setup.classList.remove('hidden');
+//   }
+// });
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+// setupClose.addEventListener('click', function () {
+//   setup.classList.add('hidden');
+// });
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    closePopup();
+  }
+});
+// setupClose.addEventListener('keydown', function(evt) {
+//   if (evt.key === 'Enter') {
+//     setup.classList.add('hidden');
+//   }
+// });
+
+
+
+var userNameInput = setup.querySelector('.setup-user-name');
+userNameInput.addEventListener('invalid', function(evt){
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+  } else if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+userNameInput.addEventListener('input', function(evt){
+  var target = evt.target;
+  if (target.value.length < MIN_NAME_LENGTH) {
+    target.setCustomValidity(
+      'Имя должно состоять минимуи из ' +
+      MIN_NAME_LENGTH +
+      '-х символов'
+      );
+  } else {
+    target.setCustomValidity('');
+  }
+});
